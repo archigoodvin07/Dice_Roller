acb2e2e0b7eb95085f14ee396d3f1dce70d34a66
archigoodvin07 <archigoodvin07@gmail.com>
2022-03-02 20:07:41 +0200

    use lateinit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
            rollDice()
         }
+            diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -30,8 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-    val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
